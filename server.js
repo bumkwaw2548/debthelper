cfc141260b94864779610449af4cbe63ea6bbad2
bumkwaw2548 <bumkwaw2548@gmail.com>
2026-04-04 01:35:13 +0700

    Final fix paths

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // React Router — ทุก route ที่ไม่ใช่ไฟล์ ให้ส่ง index.html กลับไป
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
